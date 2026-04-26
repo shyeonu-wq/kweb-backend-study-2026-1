e818f1cb8593ded907e3681fba2975a0678644d9
shyeonu-wq <shyeonu@korea.ac.kr>
2026-04-26 07:44:14 +0000

    week 1 exercise

--- week1/index.js
+++ week1/index.js
@@ -1,1 +1,15 @@
 const express = require("express");
+
+const app = express();
+
+app.get("/food", (req, res) => {
+    res.send("<h1>Hi, Human!</h1>");
+});
+
+app.get("/burger", (req, res) => {
+    res.send("<h1>Im hungry!</h1>");
+});
+
+app.listen(8080, () => {
+    console.log('Server listening on port 3000!');
+});
